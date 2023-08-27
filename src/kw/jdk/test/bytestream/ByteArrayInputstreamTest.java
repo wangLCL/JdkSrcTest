62220470df1d29e9a7d34b790b208b33e93f17c5
kangwang <2818815189@qq.com>
2023-08-27 14:25:24 +0800

    FileInputStream

---
+++ src/kw/jdk/test/bytestream/ByteArrayInputstreamTest.java
@@ -0,0 +1,9 @@
+package kw.jdk.test.bytestream;
+
+/**
+ * 实现和StringBufferInputStream类似
+ */
+public class ByteArrayInputstreamTest {
+
+
+}
---
+++ src/kw/jdk/test/bytestream/FileInputStreamTest.java
@@ -0,0 +1,24 @@
+package kw.jdk.test.bytestream;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.channels.FileChannel;
+
+/**
+ * 字符来源于文件
+ *
+ * 文件句柄是传给channel使用的
+ */
+public class FileInputStreamTest {
+    @Test
+    public void test01(){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(".gitignore");
+            System.out.println(fileInputStream.read());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
